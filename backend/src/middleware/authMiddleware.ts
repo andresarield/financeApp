69e37577ba9d430fbf9f0eb10d3272414bd16133
unknown <dabo-st@hotmail.com>
2025-03-21 23:46:54 -0300

    middleware added

---
+++ backend/src/middleware/authMiddleware.ts
@@ -0,0 +1,16 @@
+// src/middleware/authMiddleware.ts
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+export const authenticate = (req: Request, res: Response, NextFunction) => {
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+  if (!token) return res.status(401).json({ error: 'Acceso denegado' });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(400).json({ error: 'Token inválido' });
+  }
+};
