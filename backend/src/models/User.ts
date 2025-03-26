cd884dab38f3c5ae00d29dd29b32e65d76e5af03
unknown <dabo-st@hotmail.com>
2025-03-26 00:03:14 -0300

    Model of User added

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,5 +1,20 @@
-export interface User {
-    id: number;
-    username: string;
-    password: string;
-  }
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+interface UserDocument extends mongoose.Document {
+  username: string;
+  password: string;
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+});
+
+// Método para comparar contraseñas
+userSchema.methods.comparePassword = async function (candidatePassword: string) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+export default mongoose.model<UserDocument>('User', userSchema);
