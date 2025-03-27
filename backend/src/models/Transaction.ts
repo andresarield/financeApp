9f42385ed9d1e718f51a92d4f89933c8a726ab6e
unknown <dabo-st@hotmail.com>
2025-03-26 22:58:49 -0300

    Transaction.ts updated

--- backend/src/models/Transaction.ts
+++ backend/src/models/Transaction.ts
@@ -1,7 +1,19 @@
-export interface Transaction {
-    id: number;
-    description: string;
-    amount: number;
-    category: string;
-    date: string;
-  }
+import mongoose from 'mongoose';
+
+interface TransactionDocument extends mongoose.Document {
+  description: string;
+  amount: number;
+  category: string;
+  date: Date;
+  user: mongoose.Types.ObjectId;
+}
+
+const transactionSchema = new mongoose.Schema({
+  description: { type: String, required: true },
+  amount: { type: Number, required: true },
+  category: { type: String, required: true },
+  date: { type: Date, default: Date.now },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+});
+
+export default mongoose.model<TransactionDocument>('Transaction', transactionSchema);
