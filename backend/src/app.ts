68f83524c5cabf8860f5202356b5fc4fa233b323
unknown <dabo-st@hotmail.com>
2025-03-27 23:35:57 -0300

    mongo sanitize to prevent NoSQL inyections

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes';
+import mongoSanitize from 'express-mongo-sanitize';
+
+app.use(mongoSanitize());
 
 app.use('/api/auth', authRoutes);
 dotenv.config();
